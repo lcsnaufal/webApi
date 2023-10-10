eab6a5f1cdaa3cad59a32e103e0a52d3002564d7
LucasNaufal <lcsqnaufal@gmail.com>
2023-10-10 19:10:22 -0300

    final model

--- src/main/java/senac/java/Domain/Users.java
+++ src/main/java/senac/java/Domain/Users.java
@@ -10,4 +10,73 @@ public class Users {
     public static String email = "";
     public static String password = "";
     public static String cpf = "";
+
+    public Users(){
+    }
+
+    public Users(String name, String lastName, int age, String Address, String email, String password, String cpf){
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+        this.email = email;
+        this.password = password;
+        this.cpf = cpf;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
 }
